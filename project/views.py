bce549ab1d3e05a1dcad8a15bc3cbb9e9b74e09a
Casey M <caseym@gmail.com>
2015-10-11 20:35:20 -0400

    updated comments on views.py

--- project/views.py
+++ project/views.py
@@ -218,7 +218,7 @@ def add_user():
                 'site_admin': 'False'
             }
 
-            # generate password
+            # generate random password
             data['password'] = create_password()
 
             # create account
