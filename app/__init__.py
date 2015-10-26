8a4810ec15fa4da4dd367840200980cafe6fa5a7
Casey M <caseym@gmail.com>
2015-10-26 19:37:49 -0400

    db support

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask.ext.stormpath import StormpathManager
+from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 
 stormpath_manager = StormpathManager()
+db = SQLAlchemy()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
     stormpath_manager.init_app(app)
+    db.init_app(app)
 
     # register blueprints
     from .accounts import accounts as accounts_blueprint
--- manage.py
+++ manage.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 import os
-from app import create_app
+from app import create_app, db
 # from app.models import User, Role
 from flask.ext.script import Manager, Shell
-# from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
-# migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 
 def make_shell_context():
     return dict(app=app)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
-# manager.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 
 @manager.command
